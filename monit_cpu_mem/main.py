a187a2949e7639b41241439448b5024cdcb6058d
Camila <bbatistacamila@gmail.com>
2024-08-22 17:55:58 -0300

    :recycle: Refactor code

--- monit_cpu_mem/main.py
+++ monit_cpu_mem/main.py
@@ -1,6 +1,3 @@
-# sudo apt install sysstat
-# ➜ pidstat -r -u -p 24038 10 > monit_fastapi.log 
-# Temos que pegar a coluna RSS e não VSZ
 import matplotlib.pyplot as plt
 
 
@@ -11,13 +8,13 @@ def main():
     cpu = {}
 
     with open('monit_fastapi.log', 'r') as arquivo:
-        #ignora a primeira linha
+        # Ignore the first line
         next(arquivo)
-        #ignora a segunda linha que é vazia
+        # Ignore the second line which is empty
         next(arquivo)
         linhas = arquivo.readlines()
         for linha in linhas:
-            # Ignora quebra de linha e a média
+            # Ignore newline and average
             if linha == '\n' or 'Average' in linha:
                 continue
 
@@ -39,31 +36,31 @@ def main():
                 memory_x.append(list_linha[0])
                 memory_y.append(round(int(list_linha[7])/1024, 2))
 
-    # Combine e ordene os valores do eixo X
+    # Combine and sort the X-axis values
     all_x_values = sorted(set(x for v in cpu.values() for x in v['x']))
 
-    # Função para alinhar os valores de y com os valores combinados e ordenados de x
+    # Function to align the y-values with the combined and sorted x-values
     def align_series(x, y, all_x):
         y_dict = dict(zip(x, y))
         return [float(y_dict.get(x, 'nan').replace(',', '.')) if y_dict.get(x) is not None else float('nan') for x in all_x]
 
-    # Criação da figura com dois subplots
+    # Creating the figure with two subplots
     fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 12))
 
     for key, value in cpu.items():
         serie_y_aligned = align_series(cpu[key]['x'], cpu[key]['y'], all_x_values)
         ax1.plot(all_x_values, serie_y_aligned, marker='o')
         x_values = value['x']
-        y_values = [float(y.replace(',', '.')) for y in value['y']]  # Convertendo para float e trocando vírgula por ponto
+        y_values = [float(y.replace(',', '.')) for y in value['y']]  # Converting to float and replacing comma with a dot
         ax1.plot(x_values, y_values, label=f'CPU {key}', marker='o')
 
-    # Gráfico para a Série 1
+    #  Graph for Series 1
     ax1.set_title('CPU Consume')
     # ax1.set_xlabel('Time')
     ax1.set_ylabel('%')
     ax1.legend()
     ax1.grid(True)
-    ax1.set_xticks(all_x_values)  # Define os rótulos do eixo X explicitamente
+    ax1.set_xticks(all_x_values)  # Set the X-axis labels explicitly
     ax1.set_xticklabels(all_x_values, rotation=35, ha='right')
 
     ax2.plot(memory_x, memory_y, label='Memory Consume', marker='o')
@@ -72,7 +69,7 @@ def main():
     ax2.set_ylabel('MB')
     ax2.legend()
     ax2.grid(True)
-    ax2.set_xticks(memory_x)  # Define os rótulos do eixo X explicitamente
+    ax2.set_xticks(memory_x)  # Set the X-axis labels explicitly
     ax2.set_xticklabels(memory_x, rotation=35, ha='right')
 
     plt.tight_layout()
